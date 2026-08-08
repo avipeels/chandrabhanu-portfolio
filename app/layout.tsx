8d75a90959aa75196b19ee53dfd6e9f93b0b84b9
Avinash Peelukhana <avinashpsk@gmail.com>
2026-08-08 20:49:26 +0530

    Add overflow-x-hidden to body for improved layout handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       lang="en"
       className={`${montserrat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col overflow-x-hidden">{children}</body>
     </html>
   );
 }
